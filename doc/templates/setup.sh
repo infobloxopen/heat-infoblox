995ecea3d30ecb81234b2b2ba099a273eb230534
John Belamaric <jbelamaric@infoblox.com>
2015-09-30 21:43:12 -0400

    Modify setup.sh to work for Juno

--- doc/templates/setup.sh
+++ doc/templates/setup.sh
@@ -23,19 +23,19 @@ TENANT_ID=$(openstack project show $TENANT_NAME -f value -c id)
 neutron net-create --tenant-id $TENANT_ID management-net
 neutron net-create --tenant-id $TENANT_ID service-net
 
-neutron subnet-create --name management management-net --tenant-id $TENANT_ID 10.1.0.0/24
-neutron subnet-create --name service service-net --tenant-id $TENANT_ID 10.2.0.0/24
+cat <<EOF
+The rest of this stuff doesn't work in Juno, you should switch to the nios user
+and do it all manually:
 
-neutron router-create router --tenant-id $TENANT_ID
+neutron subnet-create --name management management-net 10.1.0.0/24
+neutron subnet-create --name service service-net 10.2.0.0/24
+
+neutron router-create router
 neutron router-gateway-set router public
 neutron router-interface-add router service
 
-SEC_GROUPS=$(neutron security-group-list -f value -c id -c name | cut -f 1 -d ' ')
-for id in $SEC_GROUPS
-do
-#   echo "Checking for $TENANT_ID in $id"
-   if neutron security-group-show $id | grep -q $TENANT_ID ; then
-     neutron security-group-rule-create --direction ingress $id
-   fi
-done
+neutron security-group-rule-create --direction ingress default
+
+
+EOF
 
